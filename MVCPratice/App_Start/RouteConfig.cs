25ea4bef2cf152a400d731fd5e20cefaca3cc1e3
Andy-PC\Andy <gn02618594@gmail.com>
2020-06-08 22:46:07 +0800

    feat: 新增 tests Models 以及 Controllers

--- MVCPratice/App_Start/RouteConfig.cs
+++ MVCPratice/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVCPratice
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "tests", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MVCPratice/Controllers/testsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCPratice.Models;
+
+namespace MVCPratice.Controllers
+{
+    public class testsController : Controller
+    {
+        private ProgrammingTestEntities1 db = new ProgrammingTestEntities1();
+
+        // GET: tests
+        public ActionResult Index()
+        {
+            return View(db.test.ToList());
+        }
+
+        // GET: tests/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            test test = db.test.Find(id);
+            if (test == null)
+            {
+                return HttpNotFound();
+            }
+            return View(test);
+        }
+
+        // GET: tests/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tests/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,CDE,NAM")] test test)
+        {
+            if (ModelState.IsValid)
+            {
+                db.test.Add(test);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(test);
+        }
+
+        // GET: tests/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            test test = db.test.Find(id);
+            if (test == null)
+            {
+                return HttpNotFound();
+            }
+            return View(test);
+        }
+
+        // POST: tests/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CDE,NAM")] test test)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(test).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(test);
+        }
+
+        // GET: tests/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            test test = db.test.Find(id);
+            if (test == null)
+            {
+                return HttpNotFound();
+            }
+            return View(test);
+        }
+
+        // POST: tests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            test test = db.test.Find(id);
+            db.test.Remove(test);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
